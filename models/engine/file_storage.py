53159ac6c836a1efbaf1986e271648b5fd2a4ad0
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-21 09:39:39 -0700

    update filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,11 +55,11 @@ class FileStorage:
                     'Review': Review
                   }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, val in (json.load(f)).items():
+                    val = eval(val["__class__"])(**val)
+                    self.__objects[key] = val
+                    """self.all()[key] = classes[val['__class__']](**val)"""
         except FileNotFoundError:
             pass
 
