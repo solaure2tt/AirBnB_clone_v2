59c1722e8aa2b9fe8c815b2af1130554bc30f209
Laure Tchuente <solaure2t@yahoo.fr>
2023-06-21 08:22:48 -0700

    update task 6

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 import MySQLdb
 import shlex
+from os import getenv
 from sqlalchemy.orm import relationship
 
 
@@ -14,20 +15,21 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        cities = []
-        tmp = []
-        for key in models.storage.all():
-            ci = key.replace('.', ' ')
-            city = shlex.split(ci)
-            if (city[0] == 'City'):
-                ms = models.storage.all()
-                tmp.append(ms[key])
-        for obj in tmp:
-            if (obj.state_id == self.id):
-                cities.append(obj)
-        return (cities)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                              backref="state")
+    else:
+        @property
+        def cities(self):
+            cities = []
+            tmp = []
+            for key in models.storage.all():
+                ci = key.replace('.', ' ')
+                city = shlex.split(ci)
+                if (city[0] == 'City'):
+                    ms = models.storage.all()
+                    tmp.append(ms[key])
+            for obj in tmp:
+                if (obj.state_id == self.id):
+                    cities.append(obj)
+            return (cities)
