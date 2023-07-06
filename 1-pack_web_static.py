e7086ced877fbdb227ef3ff390e30bafb220c46d
solaure2tt <solaure2t@yahoo.fr>
2023-07-06 09:47:17 -0700

    task 1 upd time create a tar file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,14 +16,10 @@ from datetime import datetime
 def do_pack():
     """create a tar file of the folder web_static"""
     da = "versions/web_static"
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d%H%M%S")
+    ds = "%s_%s.tgz" % (da, dt_string)
     """ds = "%s_%s.tgz" % (da, time.strftime("%Y%m%d%H%M%S", time.gmtime()))"""
-    dt = datetime.utcnow()
-    ds = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
