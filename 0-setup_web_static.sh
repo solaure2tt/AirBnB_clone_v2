43ede45308e822c45caa8daa2a623bfa31798cb3
solaure2tt <solaure2t@yahoo.fr>
2023-07-06 08:47:34 -0700

    task 0 fabric

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Bash script that sets up my web servers for the deployment of web_static
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Hello server!" > /data/web_static/releases/test/index.html
+ln -sF /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:group /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
